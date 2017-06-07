b4b4256cf86cbbfc6becb5b8c44bd2f3bc4834f1
Iris Robot <bluemix@iris-bot.com.br>
2017-06-07 05:07:59 +0000

    fix FB error handler

--- facebook2.js
+++ facebook2.js
@@ -66,7 +66,7 @@ exports.fbSession = function(config){
 				THIS.httprequest.post({
 					url: 'https://www.facebook.com/photos/tagging/recognition/?dpr=1.5',
 					headers: headers,
-					body: 'recognition_project=composer_facerec&photos[0]=' + imgId + '&target&is_page=false&include_unrecognized_faceboxes=true&include_face_crop_src=true&include_recognized_user_profile_picture=false&include_low_confidence_recognitions=true&' + req_parms,
+					body: 'recognition_project=composer_facerec&photos[0]=' + imgId + '&target&is_page=false&include_unrecognized_faceboxes=true&include_face_crop_src=true&include_recognized_user_profile_picture=true&include_low_confidence_recognitions=true&' + req_parms,
 					gzip: true
 				}, function(err, httpResponse, body) {
 					if(err){
@@ -140,21 +140,22 @@ exports.fbSession = function(config){
 									error: {"message": 'Facebook couldn\'t detect any face.',
 									code: -501}
 								});
-							} else if (result[0].faceboxes[0].recognitions.length == 0) {
+							} else if (result.faceboxes[0].recognitions.length == 0) {
 								_callback({
 									error: {"message": 'Facebook couldn\'t recognize this picture.',
 									code: -502}
 								});
-							} else if (result[0].faceboxes[0].recognitions[0].certainty < 0.85) {
+							} else if (result.faceboxes[0].recognitions[0].certainty < 0.85) {
 								_callback({
 									error: {"message": 'Facebook recognition has a low certainty for this picture.',
 									code: -503}
 								});
 							} else {
 								var mdata = {
-									certainty: result[0].faceboxes[0].recognitions[0].certainty,
-									name: result[0].faceboxes[0].recognitions[0].user.name,
-									fbid: result[0].faceboxes[0].recognitions[0].user.fbid
+									certainty: result.faceboxes[0].recognitions[0].certainty,
+									name: result.faceboxes[0].recognitions[0].user.name,
+									fbid: result.faceboxes[0].recognitions[0].user.fbid,
+									profile_picture: result.faceboxes[0].recognitions[0].user.profile_picture
 								};
 								THIS.graph.get(mdata.fbid + "?fields=id,name,gender,hometown,education,birthday,email,interested_in,link,relationship_status,devices",
 									function(err, _res) {
